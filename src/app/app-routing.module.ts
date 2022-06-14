6f91b54b2238fb534ef758bdaef03c89305e2351
gga1503 <graciela.angeline@binus.ac.id>
2022-06-14 19:40:29 +0700

    profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from './pages/home/home.component';
 
 import {LoginComponent} from "./pages/login/login.component";
 import {SignUpComponent} from "./pages/sign-up/sign-up.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: SignUpComponent},
+  {path: 'profile', component: ProfileComponent},
   {
     path: 'hospitals',
     loadChildren: () => import('./pages/hospitals/hospitals.module').then(m => m.HospitalsModule)
